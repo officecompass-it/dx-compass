68d8a3407ef12e96fd62122da8258f80a90c09d3
officecompass-it <office.compass.it@gmail.com>
2025-10-31 20:31:02 +0900

    Fix: フッターのSNSアイコンの修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,17 @@
 import { SnsIcons } from './SnsIcons';
+import { getProfile } from '@/lib/microcms';
 
-export const Footer = () => {
+export const Footer = async () => {
+  const profile = await getProfile();
+  
   return (
     <footer className="bg-gray-800 text-white py-8">
       <div className="container mx-auto text-center">
-        <SnsIcons className="mb-6" useDefaults={true} />
+        <SnsIcons 
+          xUrl={profile.x_url} 
+          youtubeUrl={profile.youtube_url}
+          className="mb-6" 
+        />
         <p className="text-sm">
           &copy; {new Date().getFullYear()} DXの羅針盤. All rights reserved.
         </p>
